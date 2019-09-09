be288ce23733f9c5763e7dc471fec718abe2bad2
Emily <mle.jo.11@gmail.com>
2019-09-09 14:39:54 -0500

    new erb files

--- slackapp/app/controllers/user_controller.rb
+++ slackapp/app/controllers/user_controller.rb
@@ -1,3 +1,15 @@
 class UserController < ApplicationController
     
+    def new
+    end
+
+    def create
+        User.create({
+          name: params[:name],
+          age: params[:age],
+          password: params[:password]
+        })
+        redirect_to("/user")
+      end
+
 end
